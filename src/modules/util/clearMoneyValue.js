3fe4ac98152ccdbe4069239fcf28d7cfd7ae0e9f
Leonardo Pessoa <leonardopessoa799@gmail.com>
2022-12-22 10:54:24 -0300

    Delete src directory

--- src/modules/util/clearMoneyValue.js
+++
@@ -1,5 +0,0 @@
-export default function clearMoneyValue(value) {
-  return Number(
-    value.replace(/\./g, '').replace(',', '.').replace('R$&nbsp;', '').replace('R$ ', ''),
-  ).toFixed(2);
-}
--- src/modules/util/format.js
+++
@@ -1,6 +0,0 @@
-export default function formatPrice(price) {
-  return new Intl.NumberFormat('pt-br', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(price);
-}
